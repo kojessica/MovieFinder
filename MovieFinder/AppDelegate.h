38de2392a4cff83168d85899b39fb715157ed119
Jessica Ko <ko.jessica@gmail.com>
2014-03-13 11:00:37 -0700

    Initial Commit

---
+++ MovieFinder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieFinder
+//
+//  Created by Jessica Ko on 3/13/14.
+//  Copyright (c) 2014 Jessica Ko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
