64986d2c8af9b22406b8e7268f4e54e1e93d6823
Jessica Ko <ko.jessica@gmail.com>
2014-03-15 18:47:32 -0700

    movie detail view

--- MovieFinder/MovieDetailViewController.m
+++ MovieFinder/MovieDetailViewController.m
@@ -31,6 +31,53 @@
     self.navigationItem.title = [self.movieDetail objectForKey:@"title"];
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc] init];
     self.navigationItem.backBarButtonItem = backButton;
+    
+    [self showDetail];
+}
+
+- (void)showDetail
+{
+    NSDictionary *poster = [self.movieDetail objectForKey:@"posters"];
+    NSString *thumbnail = [poster objectForKey:@"original"];
+    NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:thumbnail]];
+    UIImage *image = [[UIImage alloc] initWithData:imageData];
+    UIImageView *movieImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 30, 330, image.size.height/image.size.width*300)];
+    movieImage.image = image;
+    [self.view addSubview:movieImage];
+    
+    UIView *transparent = [[UIView alloc] initWithFrame:CGRectMake(0, 300, 330, 1000)];
+    transparent.layer.opacity = 0.9f;
+    transparent.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:transparent];
+    
+    UILabel *summary = [[UILabel alloc] initWithFrame:CGRectMake(15, 350, 300, 20)];
+    summary.text = [self.movieDetail objectForKey:@"synopsis"];
+    
+    UILabel *summaryTitle = [[UILabel alloc] initWithFrame:CGRectMake(15, 320, 300, 20)];
+    summaryTitle.text = @"Summary";
+
+    UILabel *castTitle = [[UILabel alloc] initWithFrame:CGRectMake(15, 440, 300, 20)];
+    castTitle.text = @"Cast";
+    
+    NSArray *castArray = [self.movieDetail objectForKey:@"abridged_cast"];
+    NSMutableArray *castMutArray = [[NSMutableArray alloc] init];
+    for (NSDictionary *cast in castArray) {
+        [castMutArray addObject:[cast objectForKey:@"name"]];
+    }
+    NSString *castString = [castMutArray componentsJoinedByString:@", "];
+    UILabel *cast = [[UILabel alloc] initWithFrame:CGRectMake(15, 470, 300, 20)];
+    cast.text = castString;
+    [summary setFont:[UIFont fontWithName: @"ProximaNovaRegular" size: 12]];
+    [summary setNumberOfLines:5];
+    [summary sizeToFit];
+    [cast setFont:[UIFont fontWithName: @"ProximaNovaRegular" size: 12]];
+    [cast setNumberOfLines:0];
+    [cast sizeToFit];
+    [self.view addSubview:summary];
+    [self.view addSubview:summaryTitle];
+    [self.view addSubview:cast];
+    [self.view addSubview:castTitle];
+
 }
 
 - (void)didReceiveMemoryWarning
